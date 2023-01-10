201dbdc6b2a436a601a831c8baadfbc3a57bff79
Your Name <harshdeshmukh278@gmail.com>
2023-01-11 02:20:33 +0530

    updated login system

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express=require("express");
 const app=express();
 const bodyParser=require("body-parser");
 const mysql=require("mysql2");
-
+const path=require("path");
+const session=require("express-session");
 const pool=mysql.createPool({
     connectionLimit:2,
     host:process.env.HOST,
@@ -16,12 +17,37 @@ const pool=mysql.createPool({
 
 
 //MIDDLEWARES
+
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname));
+app.use(session({
+  secret:"bahubali",
+  saveUninitialized:false,
+  resave:false,
+  cookie:{httpOnly:false},
+}));
+
+app.use((req,res,next)=>
+{
+  if((req.url==="/" || req.url=="/index.html") && typeof req.session.authenticated==='undefined')
+  {
+       res.redirect("/login")   ;
+  }
+  else if((req.url==="/achievement.html" || req.url==="/aboutus.html") && typeof req.session.authenticated==='undefined')
+  {
+       res.status(404).sendFile("404.html",{root:__dirname})
+  }
+  else
+   next();
+})
+
+app.use(express.static(path.join(__dirname)));
+
+
+
+
 
 
 //REQUEST HANDLERS
-
 app.get("/",(req,res)=>
 {
     res.sendFile("index.html",{root:__dirname});
@@ -41,20 +67,14 @@ app.get("/register",(req,res)=>
       res.sendFile("register.html",{root:__dirname});
 });
 
-app.get("/aboutus",(req,res)=>
-{
-      res.sendFile("aboutus.html",{root:__dirname});
-});
 
-app.get("/achievement",(req,res)=>
-{
-      res.sendFile("achievement.html",{root:__dirname});
-});
 
 app.post("/api/register",async (req,res)=>
 {
   try{
+
   var result=await pool.query("insert into users(first_name,last_name,email,password) values(?,?,?,?)",[req.body.firstName,req.body.lastName,req.body.email,req.body.password]);
+
   res.redirect("/login");
   }
   catch(err)
@@ -71,7 +91,9 @@ app.post("/api/login",async (req,res)=>
   try{
 
   var result=await pool.query("select * from users where email=? and password=?",[req.body.email,req.body.password]);
-    if(result[0].length==0)
+  console.log(result[0].length);
+  req.session["authenticated"]="yes";
+    if(result[0].length===0)
        res.redirect("/login");
     else
         res.redirect("/");
@@ -79,17 +101,17 @@ app.post("/api/login",async (req,res)=>
   }
   catch(err)
   {
-  //  console.log(err);
+ 
    res.redirect("/login");
   }
 
 });
 
-
+app.use((req, res, next) => {
+  res.status(404).sendFile("404.html",{root:__dirname})
+});
 
 app.listen(8000,(error)=>
 {
-    if(error)
-    //  console.log(error);
-    console.log("Server running")
+    console.log("Server running");
 })
